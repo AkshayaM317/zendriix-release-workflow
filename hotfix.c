31398ef401ddbe226dff36657bc1d6ec587596f1
Akshaya Mali <akshayamali317@gmail.com>
2025-03-02 21:42:42 +0530

    Critical production fix

---
+++ hotfix.c
@@ -0,0 +1,9 @@
+On branch hotfix/urgent-fix
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	feature
+	feature2,c
+	hotfix.c
+	main,c
+
+nothing added to commit but untracked files present (use "git add" to track)
